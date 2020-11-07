43617a5f8f0dba5bc0e38f9a04ff0c4c0bf679bf
Nana Lau <nanalau315@hotmail.com>
2020-11-07 00:29:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,7 +38,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.any? do |call|
-    valid_calls.include?(call)
+    
   end
   
 end
