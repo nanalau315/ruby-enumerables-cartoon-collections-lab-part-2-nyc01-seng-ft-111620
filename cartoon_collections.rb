7ea3ff124354d28d0f52d6a7344e279d7df06ef1
Nana Lau <nanalau315@hotmail.com>
2020-11-07 01:21:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,9 +37,9 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
-  planeteer_calls.to_set == valid_calls.to_set
-  
+  planeteer_calls.find do |word|
+    valid_calls.include?(word)
     
-  
-  
+  end
+
 end
